f0a6ab69535204bbfdb8b5eaac8b059514deb323
boy15 <boy15@KIMMER>
2024-10-13 22:43:46 +0800

    Add project files.

---
+++ matchandlearn/Pages/Alphabets.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace matchandlearn.Pages
+{
+    public class AlphabetsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
